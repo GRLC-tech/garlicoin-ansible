1b898a4562d39ea95af2d581088b77d5aee29fe3
jorge <jorge@garlicoin.io>
2022-02-22 05:44:50 -0500

    Fix banner & cert path

--- roles/electrumx/templates/startup.sh
+++ roles/electrumx/templates/startup.sh
@@ -8,14 +8,14 @@ export DAEMON_URL='http://{{ rpc_username }}:{{ rpc_password }}@127.0.0.1:42068'
 export COIN='Garlicoin'
 export HOST='0.0.0.0'
 export SSL_PORT='50002'
-export SSL_KEYFILE='/root/{{ private_key_filename }}'
-export SSL_CERTFILE='/root/{{ public_cert_filename }}'
+export SSL_KEYFILE='/data/{{ private_key_filename }}'
+export SSL_CERTFILE='/data/{{ public_cert_filename }}'
 
 #VISIBILITY
 export DONATION_ADDRESS='{{ donation_address }}'
 export REPORT_HOST='{{ ansible_host }}'
 export REPORT_SSL_PORT='50002'
-export BANNER_FILE='/root/banner.txt'
+export BANNER_FILE='/data/banner.txt'
 
 #SETTINGS
 export MAX_SESSIONS='500'
